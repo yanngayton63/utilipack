de9d26030f630e5f629b357edcefa2ee3b0eddb1
yanngayton63 <yann.gayton63@gmail.com>
2024-07-03 11:56:39 +0200

    Create DiskFileExplorer.java

---
+++ src/main/java/yga/utilipack/DiskFileExplorer.java
@@ -0,0 +1,99 @@
+package yga.utilipack;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.log4j.Logger;
+
+/**
+ * Class to explore and list the contents of a directory.
+ * Provides functionality to list all files and directories,
+ * optionally including subdirectories.
+ * 
+ * <p>
+ * Original source: http://www.fobec.com/java/964/lister-fichiers-dossiers-repertoire.html
+ * </p>
+ * 
+ * <p>
+ * Example usage:
+ * <pre>
+ *     DiskFileExplorer explorer = new DiskFileExplorer("/path/to/directory", true);
+ *     List<String> files = explorer.listDirectory("/path/to/directory");
+ *     for (String file : files) {
+ *         System.out.println(file);
+ *     }
+ * </pre>
+ * </p>
+ * 
+ * @version 1.1
+ * @since 2010
+ */
+public class DiskFileExplorer {
+
+    // Logger for logging important events
+    private static final Logger logger = Logger.getLogger(DiskFileExplorer.class);
+
+    // The initial path of the directory to be explored.
+    private String initialPath;
+
+    // Flag to determine if subdirectories should be included.
+    private Boolean recursivePath;
+
+    // Counter for the number of files found.
+    public int fileCount = 0;
+
+    // Counter for the number of directories found.
+    public int dirCount = 0;
+
+    /**
+     * Constructor to initialize DiskFileExplorer.
+     * 
+     * @param path The path of the directory to be explored.
+     * @param subFolder Boolean flag to indicate if subdirectories should be included.
+     */
+    public DiskFileExplorer(String path, Boolean subFolder) {
+        this.initialPath = path;
+        this.recursivePath = subFolder;
+        logger.info("DiskFileExplorer initialized with path: " + path + " and recursivePath: " + subFolder);
+    }
+
+    /**
+     * Method to initiate the listing of the directory from the initial path.
+     */
+    public void list() {
+        this.listDirectory(this.initialPath);
+    }
+
+    /**
+     * Recursively lists the contents of a directory.
+     * 
+     * @param dir The directory to be listed.
+     * @return A list of strings representing the absolute paths of the files.
+     */
+    public List<String> listDirectory(String dir) {
+        File file = new File(dir);
+        File[] files = file.listFiles();
+        List<String> fileList = new ArrayList<>();
+        
+        // Check if the directory is not empty
+        if (files != null) {
+            for (File f : files) {
+                if (f.isDirectory()) {
+                    dirCount++;
+                    logger.debug("Directory found: " + f.getAbsolutePath());
+                    if (this.recursivePath) {
+                        fileList.addAll(this.listDirectory(f.getAbsolutePath()));
+                    }
+                } else {
+                    fileCount++;
+                    logger.debug("File found: " + f.getAbsolutePath());
+                    fileList.add(f.getAbsolutePath());
+                }
+            }
+        } else {
+            logger.warn("The directory " + dir + " is empty or does not exist.");
+        }
+        
+        return fileList;
+    }
+}
